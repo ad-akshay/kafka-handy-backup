77def388d5de114a8d319f833e08ae52ea7c1e38
Raphael <raphael.neveu@eugeria.ca>
2022-07-12 16:08:10 -0400

    Retreive topics info

--- src/kafka-backup.py
+++ src/kafka-backup.py
@@ -1,7 +1,22 @@
 #! /bin/python3
+from typing import Dict, List
 from confluent_kafka import Producer, Consumer, TopicPartition, OFFSET_BEGINNING, OFFSET_END, OFFSET_INVALID, OFFSET_STORED
 from confluent_kafka.admin import AdminClient
 from time import sleep
+
+from dataclasses import dataclass
+
+@dataclass
+class PartitionDetails:
+    id: int
+    minOffset: int
+    maxOffset: int
+
+@dataclass
+class TopicDetails:
+    name: str
+    paritions: List[PartitionDetails]
+
 
 BOOTSTRAP_SERVERS = 'localhost:29092'
 CLIENT_ID = 'kafka-backup.py'
@@ -21,6 +36,47 @@ admin = AdminClient({
     'bootstrap.servers': BOOTSTRAP_SERVERS,
 })
 
+
+def list_consumer_groups():
+    """Return the id of all active consumer groups"""
+    group_meta = admin.list_groups(timeout=10) # List all active consumer groups
+    return [d.id for d in group_meta]
+    # print(f'- id={data.id} broker={data.broker} state={data.state} members={data.members}')
+
+
+def topics_details():
+    """Retreive topics details (paritions, etc.)"""
+
+    topics_details = []
+    cluster_meta = admin.list_topics(timeout=5) # Get info for this topic
+    for t in cluster_meta.topics.values():
+        if t.topic == "__consumer_offsets":
+            continue
+
+        partitions = []
+        for id in t.partitions:
+            # print(p.id, p.leader)
+            wm = consumer.get_watermark_offsets(TopicPartition(t.topic, id))
+            # print(f'{id} : {wm}')
+            partitions.append(PartitionDetails(id, wm[0], wm[1]))
+        
+        topics_details.append(TopicDetails(t.topic, partitions))
+
+    print(topics_details)
+
+
+def consumer_group_details():
+
+    consumer_goups = list_consumer_groups()
+
+    print('Consumer group details:')
+    for id in ['kafka-backup']: # Consumer group IDs
+        c = Consumer({
+            'group.id': id,
+            'bootstrap.servers': BOOTSTRAP_SERVERS,
+        })
+        p = c.committed([TopicPartition('test-topic', 0)])
+        # print('Comitted:', [f'topic={p.topic} offset={offsetToStr(p.offset)} partition={p.partition}' for p in partitions])
 
 def produceMessages(count = 100):
     for i in range(0, count):
@@ -77,8 +133,9 @@ def printInfo():
 if __name__ == "__main__":
     
     # consumeMessages()
-    produceMessages(200)
-    printInfo()
+    # produceMessages(123)
+    # printInfo()
+    topics_details()
 
 
     print('Closing consumer')
