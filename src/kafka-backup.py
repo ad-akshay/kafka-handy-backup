6b5bb283fe311eb6c1d803d7d42ad61b4166a2ae
Raphael <raphael.neveu@eugeria.ca>
2022-07-20 16:15:13 -0400

    Add --original-partitions and --ignore-error options for restore command.

--- src/kafka-backup.py
+++ src/kafka-backup.py
@@ -37,6 +37,8 @@ p3.add_argument('--bootstrap-servers', type=str)
 p3.add_argument('--directory', type=str, default='backup', help='Backup directory/container (default="backup")')
 p3.add_argument('--topic', '-t', dest='topics', action='append', help='Topics to backup')
 p3.add_argument('--topics-regex', type=str, help='Topics to restore')
+p3.add_argument('--original-partitions', action='store_true', help='Publish messages to their original partitions')
+p3.add_argument('--ignore-errors', action='store_true', help='Ignore topics with errors')
 
 
 # "backup-info" command parser
@@ -206,6 +208,9 @@ if __name__ == "__main__":
 
             # Cluster partitions
             t['partitions'] = [PartitionDetails(**p) for p in restoration_point_metadata.topics[t['source']]['partitions']]
+
+            if args.original_partitions and len(d.partitions) < len(t['partitions']):
+                t['error'] = f'Topic in cluster has a lower number of partitions ({len(d.partitions)}) than the backup ({len(t["partitions"])}): cannot restore original partitions for this topic.'
         
         # Print restoration summary
         topics_to_restore.sort(key=lambda x: x['source'])
@@ -215,12 +220,26 @@ if __name__ == "__main__":
         if len(errors) > 0:
             print('Errors:')
         for t in errors:
-            print(f"- {t['source']} -> {t['error']}")
+            print(f"- {t['source']} : {t['error']}")
 
         print(f'Topics to restore:')
         for t in topics_to_restore:
             for p in t['partitions']:
-                print(f"- {t['source']}:{p.id} ({p.minOffset}, {p.maxOffset}) -> {t['destination']}:any")
+                print(f"- {t['source']}:{p.id} ({p.minOffset}, {p.maxOffset}) -> {t['destination']}:{p.id if args.original_partitions else 'any'}")
+
+        if len(errors) > 0 and not args.ignore_errors:
+            print('Aborted: there are some errors. Use --ignore-errors if you wish to continue ignoring the topics with errors.')
+            exit()
+            
+        if len(topics_to_restore):
+            print('There are not topics to restore')
+            exit()
+            
+        if len(errors) > 0 and args.ignore_errors:
+            print('WARNING: Topics with errors will be ignored')
+
+        
+
 
     elif args.command == 'list-topics':
         for t in Metadata.topics_details(BOOTSTRAP_SERVERS).values():
