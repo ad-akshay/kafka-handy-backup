5e279de8866cb03c1cc964dc1fe545d1eb819155
Raphael <raphael.neveu@eugeria.ca>
2022-07-28 15:40:17 -0400

    Add encoding functions in consumer_offset_utils

--- src/consumer_offset_utils.py
+++ src/consumer_offset_utils.py
@@ -58,14 +58,14 @@ OFFSET_COMMIT_KEY_SCHEMA = [
 OFFSET_COMMIT_VALUE_SCHEMA = [
     # https://github.com/apache/kafka/blob/3.3/core/src/main/resources/common/message/OffsetCommitValue.json
     { "name": "offset", "type": "int64", "versions": "0+" },
-    { "name": "leaderEpoch", "type": "int32", "versions": "3+"},
+    { "name": "leaderEpoch", "type": "int32", "versions": "3+", "default": -1, "ignorable": True},
     { "name": "metadata", "type": "string", "versions": "0+" },
     { "name": "commitTimestamp", "type": "int64", "versions": "0+" },
-    { "name": "expireTimestamp", "type": "int64", "versions": "1"}
+    { "name": "expireTimestamp", "type": "int64", "versions": "1", "default": -1, "ignorable": True}
 ]
 
 GROUP_METADATA_KEY_SCHEMA = [
-    # https://github.com/apache/kafka/blob/3.3/core/src/main/resources/common/message/OffsetCommitValue.json
+    # https://github.com/apache/kafka/blob/3.3/core/src/main/resources/common/message/GroupMetadataKey.json
     { "name": "group", "type": "string", "versions": "2" }
   ]
 
@@ -88,7 +88,7 @@ UNPACK_MAP = {
     'uint64': { 'format':'>Q', 'size': 8 },
 }
 
-def decodeFromSchema(version: int, buffer: bytes, fields: List[Dict]):
+def decodeFromSchema(version: int, buffer: bytes, fields: List[Dict]) -> Dict:
     """Decode a buffer from the given schema (fields) following the specified version"""
     obj = { "version": version }
     cursor = 0
@@ -117,6 +117,37 @@ def decodeFromSchema(version: int, buffer: bytes, fields: List[Dict]):
 
     return obj
 
+
+def encodeFromSchema(obj: Dict, fields: List[Dict]) -> bytes:
+    """Encode a buffer from the given schema (fields) following the specified version"""
+    version = obj['version']
+    buffer = b''
+
+    for field in fields:
+        if not includes_version(version, field['versions']):
+            continue
+
+        if field['name'] not in obj:
+            if field.get('default') is not None:
+                obj[field['name']] = field.get('default')
+            else:
+                print(f"ERROR: missing {field['name']} in provided object")
+                return None
+
+        if field['type'] == 'string': # Special case for string
+            # String is encoded with the size first : | <string_size> (uint16) | <string_chars> (<string_size> bytes) |
+            buffer += pack('>H', len(obj[field['name']]))
+            buffer += obj[field['name']].encode()
+        else: # Other types can be handled by unpack()
+            l = UNPACK_MAP.get(field['type'])
+            if not l:
+                print(f'EXCEPTION: {field["type"]} is not defined in the UNPACK_MAP')
+                return None
+
+            buffer += pack(l['format'], obj[field['name']])
+
+    return buffer
+
 def decodeKey(key: bytes) -> Dict:
     """Decode the key of a message from the __consumer_offsets topic"""
     version = unpack('>H', key[0:2])[0]
@@ -125,10 +156,23 @@ def decodeKey(key: bytes) -> Dict:
     # print('schema:', schema)
     return decodeFromSchema(version, key[2:], schema)
 
+def encodeKey(key: Dict) -> bytes:
+    """Encode the key to bytes"""
+    schema = MESSAGE_TYPE_SCHEMAS[key['version']]
+    buffer = pack('>H', key['version'])
+    buffer += encodeFromSchema(key, schema)
+    return buffer
+
 def decodeOffsetValue(buffer: bytes):
     """Decode the value of a offset commit message from the __consumer_offsets topic"""
     version = unpack('>H', buffer[0:2])[0]
     return decodeFromSchema(version, buffer[2:], OFFSET_COMMIT_VALUE_SCHEMA)
+
+def encodeOffsetValue(value: Dict) -> bytes:
+    """Encode the value of a offset commit message"""
+    buffer = pack('>H', value['version'])
+    buffer += encodeFromSchema(value, OFFSET_COMMIT_VALUE_SCHEMA)
+    return buffer
 
 def decodeMetadataValue(value: bytes):
     return None # Not implemented (not useful for this project, plus not sure how to decode this part)
